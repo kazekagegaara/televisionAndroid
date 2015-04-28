2518b1c715cedcf411b8f47ad1121bf634ec3824
Akshay Ashwathanarayana <aashwath@asu.edu>
2015-04-28 14:03:33 -0700

    PRO pdf

--- TeleVisionAndroid/app/src/main/java/asu/edu/televisionandroid/survey/SurveyReviewActivity.java
+++ TeleVisionAndroid/app/src/main/java/asu/edu/televisionandroid/survey/SurveyReviewActivity.java
@@ -1,15 +1,24 @@
 package asu.edu.televisionandroid.survey;
 
 import android.content.Intent;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import asu.edu.televisionandroid.ImagePickerActivity;
@@ -41,6 +50,52 @@ public class SurveyReviewActivity extends Activity {
             public void onClick(View v) {
                 Intent myIntent = new Intent(SurveyReviewActivity.this, ImagePickerActivity.class);
                 SurveyReviewActivity.this.startActivity(myIntent);
+                LayoutInflater inflater = getLayoutInflater();
+                View pdfView = inflater.inflate(R.layout.pro_pdf_page, null, false);
+                ListView listView2 = (ListView) pdfView.findViewById(R.id.listView);
+                listView2.setAdapter(adapter);
+                // create a new document
+                PdfDocument document = new PdfDocument();
+
+                // crate a page description
+                PdfDocument.PageInfo pageInfo;
+                DisplayMetrics dm = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(dm);
+                int width=dm.widthPixels;
+                int height=dm.heightPixels;
+                pageInfo = new PdfDocument.PageInfo.Builder(width, height, 1).create();
+
+                // start a page
+                PdfDocument.Page page = document.startPage(pageInfo);
+
+                // draw something on the page
+                RelativeLayout content = (RelativeLayout)findViewById(R.id.wholeView);
+                content.draw(page.getCanvas());
+
+                // finish the page
+                document.finishPage(page);
+               /* . . .
+                // add more pages
+                . . .
+               */ // write the document content
+                String externalPath = Environment.getExternalStorageDirectory().toString();
+                File outputFolder = new File(externalPath+"/TeleVisionPRO");
+                File file = new File(outputFolder.getAbsolutePath(), "proReport.pdf");
+                if(!outputFolder.exists()){
+                    outputFolder.mkdir();
+                }
+                try {
+                    file.createNewFile();
+                    FileOutputStream outputStream = new FileOutputStream(file);
+                    document.writeTo(outputStream);
+                } catch (java.io.IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                // close the document
+                document.close();
+
             }
         });
     }
